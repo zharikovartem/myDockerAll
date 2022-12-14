9282d038ad261c0d885d0e1594647520857855d0
Artem Zharikov <a.zharikov@smartdesign.by>
2022-12-14 14:29:18 +0300

    added selectors

--- frontend/src/Components/MainPage/MainPage.tsx
+++ frontend/src/Components/MainPage/MainPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectIsAuth } from '../../Redux/Selectors/authSelector'
 import LoginForm from '../Login/LoginForm'
 
 type MainPagePropsType = {
@@ -7,42 +9,51 @@ type MainPagePropsType = {
 
 const MainPage:React.FC<MainPagePropsType> = (props) => {
 
-    return (
-        <>
-            <h1>MainPage</h1>
-            <p>Username: admin@admin.com</p>
-            <p>Password: 12345678</p>
-            <LoginForm />
-            <br/>
+    const isAuth = useSelector(selectIsAuth)
 
-            <ul>
-                <li>Перейти в новый репозиторий</li>
-            </ul>
-
-            <ul>
-                <li>(done)Создать дэфолтного юзера</li>
-                <li>(done)Реализовать логин</li>
-                <li>Реализовать регистрацию</li>
-                <li>Сделать базовое меню</li>
-                <li>Вывести базовый функционал</li>
-            </ul>
-
-            <ul>
-                <li>(done)Api-platform</li>
-            </ul>
-
-            <ul>
-                <li>Web-sockets</li>
-            </ul>
-
-            <ul>
-                <li>Clean php</li>
-                <li>Clean js</li>
-                <li>Clean yaml</li>
-                <li>Clean packages</li>
-            </ul>
-        </>
-    )
+    if (!isAuth) {
+        return (
+            <>
+                <h1>MainPage</h1>
+                <p>Username: admin@admin.com</p>
+                <p>Password: 12345678</p>
+                <LoginForm />
+                <br/>
+    
+                <ul>
+                    <li>Перейти в новый репозиторий</li>
+                </ul>
+    
+                <ul>
+                    <li>(done)Создать дэфолтного юзера</li>
+                    <li>(done)Реализовать логин</li>
+                    <li>Реализовать регистрацию</li>
+                    <li>Сделать базовое меню</li>
+                    <li>Вывести базовый функционал</li>
+                </ul>
+    
+                <ul>
+                    <li>(done)Api-platform</li>
+                </ul>
+    
+                <ul>
+                    <li>Web-sockets</li>
+                </ul>
+    
+                <ul>
+                    <li>Clean php</li>
+                    <li>Clean js</li>
+                    <li>Clean yaml</li>
+                    <li>Clean packages</li>
+                </ul>
+            </>
+        )
+    } else {
+        return (
+            <>MainPage</>
+        )
+    }
+    
 }
 
 export default  MainPage
