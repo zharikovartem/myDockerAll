c1ce87a3a194c50a4c2c24439894a80aa33062a4
Artem Zharikov <a.zharikov@smartdesign.by>
2022-12-14 15:02:28 +0300

    added menu

--- frontend/src/Components/MainPage/MainPage.tsx
+++ frontend/src/Components/MainPage/MainPage.tsx
@@ -1,59 +1,23 @@
+import { Spin } from 'antd'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import styled from 'styled-components'
 import { selectIsAuth } from '../../Redux/Selectors/authSelector'
 import LoginForm from '../Login/LoginForm'
 
 type MainPagePropsType = {
-    
+
 }
 
-const MainPage:React.FC<MainPagePropsType> = (props) => {
+const MainPage: React.FC<MainPagePropsType> = (props) => {
 
-    const isAuth = useSelector(selectIsAuth)
-
-    if (!isAuth) {
-        return (
-            <>
-                <h1>MainPage</h1>
-                <p>Username: admin@admin.com</p>
-                <p>Password: 12345678</p>
-                <LoginForm />
-                <br/>
-    
-                <ul>
-                    <li>Перейти в новый репозиторий</li>
-                </ul>
-    
-                <ul>
-                    <li>(done)Создать дэфолтного юзера</li>
-                    <li>(done)Реализовать логин</li>
-                    <li>Реализовать регистрацию</li>
-                    <li>Сделать базовое меню</li>
-                    <li>Вывести базовый функционал</li>
-                </ul>
-    
-                <ul>
-                    <li>(done)Api-platform</li>
-                </ul>
-    
-                <ul>
-                    <li>Web-sockets</li>
-                </ul>
-    
-                <ul>
-                    <li>Clean php</li>
-                    <li>Clean js</li>
-                    <li>Clean yaml</li>
-                    <li>Clean packages</li>
-                </ul>
-            </>
-        )
-    } else {
-        return (
-            <>MainPage</>
-        )
-    }
-    
+    return (
+        <>
+            <h1>Main Page</h1>
+            <h3>Parsers list:</h3>
+            <li>Onliner</li>
+        </>
+    )
 }
 
-export default  MainPage
+export default MainPage
