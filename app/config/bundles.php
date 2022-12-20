d3120316a50aee25c03c85d16e93e6336b84292b
Artem Zharikov <a.zharikov@smartdesign.by>
2022-12-20 23:54:16 +0300

    installed rabbit

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
 ];
