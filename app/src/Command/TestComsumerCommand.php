3b88103bb7e9fbe914cac70e7255395b6098f173
Artem Zharikov <a.zharikov@smartdesign.by>
2023-01-04 19:38:11 +0300

    rabbit and worker ok

--- app/src/Command/TestComsumerCommand.php
+++ app/src/Command/TestComsumerCommand.php
@@ -3,25 +3,32 @@
 namespace App\Command;
 
 use App\Repository\CatalogRepository;
+use App\Service\RabbitService;
+use OldSound\RabbitMqBundle\RabbitMq\ProducerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-// clear; bin/console app:create-directories
+// clear; bin/console app:test-consumer
 #[AsCommand(name: 'app:test-consumer')]
 class TestComsumerCommand extends Command
 {
 
     // bin/console rabbitmq:consumer parse -vvv
     // bin/console app:test-consumer
+
+    public function __construct(
+        private readonly RabbitService $rabbitService,
+    ) {
+        parent::__construct();
+    }
     
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // dd('!!!');
-        $this->container->get('old_sound_rabbit_mq.parse_producer')->publish('Сообщенька для отправки на мыло...');
-        // $this->container->get('old_sound_rabbit_mq.send_email_producer')->publish('Сообщенька для отправки на мыло...');
-        // 
+        $result = $this->rabbitService->publishParseQueue([
+            'message' => 'TEST'
+        ]);
         
         return Command::SUCCESS;
     }
--- app/src/Consumer/ParseConsumer.php
+++ app/src/Consumer/ParseConsumer.php
@@ -5,11 +5,13 @@ namespace App\Consumer;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 
-class ParseConsumer extends ConsumerInterface
+class ParseConsumer implements ConsumerInterface
 {
-    public function execute(AMQPMessage $msg)
+    public function execute(AMQPMessage $msg): bool
     {
         echo 'Ну тут типа сообщение пытаюсь отправить: '.$msg->getBody().PHP_EOL;
         echo 'Отправлено успешно!...';
+
+        return true;
     }
 }
